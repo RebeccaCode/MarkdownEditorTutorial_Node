a3a33f94ab119edfe224b375e857558bc4280add
Rebecca Haskins <rebecca@rebeccahaskins.com>
2018-06-13 15:13:06 -0400

    Added code for sharejs and redis
    Redis is not native to Windows -- need to understand the Windows Redis port before this will work.

--- server.js
+++ server.js
@@ -11,8 +11,23 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.render('pad');
 });
+app.get('/(:id)', function(req,res) {
+  res.render('pad');
+});
 
-//listen on port 8000 (for localhost) or the port defined for heroku
+// get sharejs dependencies
+var sharejs = require('share');
+require('redis');
+
+//options for sharejs
+var options = {
+  db: {type: 'redis'},
+};
+
+// attach the express server to sharejs
+sharejs.server.attach(app, options);
+
+//listen on specified port
 var port = process.env.PORT || 8190;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
